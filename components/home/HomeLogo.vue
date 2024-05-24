caca2ef7699602f979a7a831fd8e66f253cafcbb
lukaszzm <lukasz.malucha1@gmail.com>
2024-05-24 14:32:49 +0200

    fix: Home page logo

--- components/home/HomeLogo.vue
+++ components/home/HomeLogo.vue
@@ -2,7 +2,7 @@
   <UButton
     to="/"
     variant="link"
-    class="flex items-center gap-1 hover:no-underline hover:text-primary"
+    class="flex items-center gap-1 hover:no-underline text-gray hover:text-gray"
   >
     <UIcon name="i-lucide-gantt-chart" class="size-10 text-primary" />
     <span class="font-semibold text-2xl">Syncro</span>
