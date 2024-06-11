aed47b82c977b13c7b07f0d258d0673de9d5d628
lukaszzm <lukasz.malucha1@gmail.com>
2024-06-11 16:26:16 +0200

    feat: Add tasks not found info

--- components/task/TaskList.vue
+++ components/task/TaskList.vue
@@ -15,9 +15,7 @@ defineProps<Props>();
       <h2 class="text-lg font-medium text-gray-600">Tasks:</h2>
       <TaskNew :project-id="projectId" />
     </div>
-    <p v-if="tasks.length === 0" class="p-4 text-gray-400 font-medium">
-      No tasks...
-    </p>
+    <TaskNotFound v-if="tasks.length === 0" />
     <div v-else class="space-y-2">
       <TaskItem v-for="task in tasks" :key="task.id" v-bind="task" />
     </div>
---
+++ components/task/TaskNotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="text-gray-600 font-medium flex items-center justify-center flex-col max-w-xs"
+  >
+    <UIcon name="i-lucide-rabbit" class="text-2xl" />
+    <p>No tasks</p>
+  </div>
+</template>
